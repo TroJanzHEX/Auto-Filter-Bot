ffd1551460910cc0ccff9173a86eb137fbc847a6
prgofficial <65294906+prgofficial@users.noreply.github.com>
2021-03-02 12:48:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,27 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
+# Get a bot token from botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+# Get from my.telegram.org (or @UseTGXBot)
 APP_ID = int(os.environ.get("APP_ID", ""))
+
+# Get from my.telegram.org (or @UseTGXBot)
 API_HASH = os.environ.get("API_HASH", "")
+
+# Generate a user session string 
 TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
-#######################################
+
+# ID of Channel from which the bot shoul search files
+LOGCHANNEL_ID = os.environ.get("LOGCHANNEL_ID", "")
+
+
+
+
 TG_BOT_SESSION = os.environ.get("TG_BOT_SESSION", "bot")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 LOG_FILE_NAME = "filterbot.txt"
-LOGCHANNEL_ID = os.environ.get("LOGCHANNEL_ID", "")
 
 logging.basicConfig(
     level=logging.INFO,
